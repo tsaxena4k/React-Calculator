7bf683a5b59e41e9d4a18b4ef2b2922c43c84025
Tushar saxena <46051734+tsaxena4k@users.noreply.github.com>
2020-09-02 12:32:35 +0530

    Updates

---
+++ precache-manifest.72727a59b55c20506689e00cebebb0c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ec7b9df2f7d894d4cefd85269f71a0",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "3212ce5b98fb647f6a1c",
+    "url": "/React-Calculator/static/css/main.20c46901.chunk.css"
+  },
+  {
+    "revision": "52a56fd254485ade0501",
+    "url": "/React-Calculator/static/js/2.bbbe7de9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.bbbe7de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3212ce5b98fb647f6a1c",
+    "url": "/React-Calculator/static/js/main.3dd75da5.chunk.js"
+  },
+  {
+    "revision": "a0839f86b5efc7a12679",
+    "url": "/React-Calculator/static/js/runtime-main.b9245783.js"
+  }
+]);
